f507b39a72737008d1db4cda06f092e86d0465b5
xeroman.kyn <xeroman.kyn@aceenter.com>
2023-02-07 22:48:53 +0900

    study 230207

--- src/ch05/class04/Main.java
+++ src/ch05/class04/Main.java
@@ -1,13 +1,41 @@
 package ch05.class04;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
 
-    public String solution(String str) {
-        String answer = "";
-        System.out.println(str);
-        return answer;
+    public int solution(String str) {
+//        352+*9-
+
+        char[] arr = str.toCharArray();
+        Stack<Integer> s2 = new Stack<>();
+        for (char c : arr) {
+            if (Character.isDigit(c)) {
+                s2.push(Integer.parseInt(String.valueOf(c)));
+            } else {
+                int r = s2.pop();
+                int l = s2.pop();
+                int tmp = 0;
+                switch (c) {
+                    case '+':
+                        tmp = l + r;
+                        break;
+                    case '-':
+                        tmp = l - r;
+                        break;
+                    case '*':
+                        tmp = l * r;
+                        break;
+                    case '/':
+                        tmp = l / r;
+                        break;
+                }
+                s2.push(tmp);
+            }
+        }
+
+        return s2.pop();
     }
 
     public static void main(String[] args) {
