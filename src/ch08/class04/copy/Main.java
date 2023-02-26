493b9db90fd03ed0c99cf203f2dbd2e376b0137d
xeroman.kyn <xeroman.k@gmail.com>
2023-02-26 22:59:16 +0900

    study 230226 이걸 BFS라고 해야하나 완전탐색이라고 보는게 맞을것 같은데. 여튼 이건 BFS가 좀 더 빠름. 내친김에 완전 탐색을... 이건 또 완전탐색이라고 해야하나? 간단할 거라 생각했지만, 정해진 크기가 아니라 입력받는 수에 따라 배열 크기가 달라질 수 있어서, 생각보다 힘들었음... 물론 이게 더 빠름;

--- src/ch08/class04/copy/Main.java
+++ src/ch08/class04/copy/Main.java
@@ -1,19 +1,20 @@
 package ch08.class04.copy;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 class ForBFS{
 
-    String r = "";
-    int lv = 0;
-    int cnt = 0;
-    int[][] chk;
-    public ForBFS(int v, int n, int m){
-        r = v + " ";
-        lv = 1;
-        chk = new int[m][n];
+    String r;
+    int cnt;
+    int v;
+    public ForBFS(int cnt, int v, String r){
+        this.r = r;
+        this.cnt = cnt;
+        this.v = v;
     }
 
 }
@@ -21,35 +22,89 @@ public class Main {
 
     public void solution(int n, int m) {
 
-//        solDFS(n, m, "", 0);
+        long t1 = System.currentTimeMillis();
+
+        solDFS(n, m, "", 0);
+        long t2 = System.currentTimeMillis();
 
         solBFS(n, m);
+        long t3 = System.currentTimeMillis();
+
+        solExSearch(n, m) ;
+        long t4 = System.currentTimeMillis();
+
+        System.out.println(t2 - t1);
+        System.out.println(t3 - t2);
+        System.out.println(t4 - t3);
     }
+    public void solExSearch(int n, int m){
+        Integer[] arr = new Integer[m];
+        for(int i=0; i<m; i++){
+            arr[i] = n;
+        }
+
+        Stack<String> s = new Stack<>();
+
+        int currPoint = m - 1;
+        while(arr[currPoint] > -1){
+            if(arr[currPoint] == 0){
+                arr[currPoint] = n;
+                currPoint--;
+                if(currPoint < 0) break;
+                arr[currPoint]--;
+
+                continue;
+            }
+
+            currPoint = m - 1;
+            s.push(String.join(" ", Arrays.stream(arr).map(o -> o.toString()).toArray(String[]::new)));
+            arr[currPoint]--;
+        }
+
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+
+    }
+
 
     public void solBFS(int n, int m){
 
         for(Integer i=1; i<=n; i++){
             Queue<ForBFS> q = new LinkedList<>();
-            q.offer(new ForBFS(i));
+            q.offer(new ForBFS(1, i, i + " "));
+
             while (!q.isEmpty()){
                 int len = q.size();
                 for(int j=0; j<len; j++){
                     ForBFS f = q.poll();
+
+                    if(f.cnt >= m){
+                        System.out.println(f.r);
+                        continue;
+                    }
+
+                    for(Integer t=1; t<=n; t++){
+                        String temp = f.r + t + " ";
+                        int tempCnt = f.cnt + 1;
+                        q.offer(new ForBFS(tempCnt, i, temp));
+                    }
+
                 }
             }
 
         }
     }
 
-    public void solDFS(int n, int m, String r, int idx){
+    public void solDFS(int n, int m, String r, int cnt){
 
-        if(idx == m){
+        if(cnt == m){
             System.out.println(r);
             return;
         }
 
         for(int i=0; i<n; i++){
-            solDFS(n, m, r + (i+1) + " ",idx+1);
+            solDFS(n, m, r + (i+1) + " ",cnt+1);
         }
     }
 
