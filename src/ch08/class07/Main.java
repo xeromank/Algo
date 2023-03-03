3321b8ed05c6276c370cda772ba1c9cdf5f8b899
xeroman.kyn <xeroman.k@gmail.com>
2023-03-03 16:45:20 +0900

    230303 메모리제이션...BFS

--- src/ch08/class07/Main.java
+++ src/ch08/class07/Main.java
@@ -1,6 +1,18 @@
 package ch08.class07;
 
 import java.util.Scanner;
+import java.util.Stack;
+
+class Node {
+
+    int n;
+    int r;
+
+    public Node(int n, int r) {
+        this.n = n;
+        this.r = r;
+    }
+}
 
 public class Main {
 
@@ -10,20 +22,54 @@ public class Main {
         return answer;
     }
 
-    public int DFS(int n, int r){
+    public int DFS(int n, int r) {
 
-        if(n == r) return 1;
-        if(r == 0) return 1;
-
-
-        if(this.arr[n-1][r-1] == 0){
-            return this.arr[n-1][r-1] = DFS(n-1, r-1) + DFS(n-1, r);
+        if (n == r) {
+            return 1;
+        }
+        if (r == 0) {
+            return 1;
         }
 
-        return this.arr[n-1][r-1];
+        if (this.arr[n - 1][r - 1] == 0) {
+            return this.arr[n - 1][r - 1] = DFS(n - 1, r - 1) + DFS(n - 1, r);
+        }
+
+        return this.arr[n - 1][r - 1];
+    }
+
+    public int BFS(int n, int r) {
+
+        Stack<Node> s = new Stack<>();
+
+        s.push(new Node(n, r));
+
+        while (!s.isEmpty()) {
+
+            Node node = s.pop();
+
+            if(node.n == node.r || node.r == 0){
+                this.arrBFS[node.n ][node.r] = 1;
+                continue;
+            }
+
+            if (this.arrBFS[node.n - 1][node.r - 1] == 0) {
+                s.push(node);
+                s.push(new Node(node.n - 1, node.r - 1));
+            } else if (this.arrBFS[node.n - 1][node.r] == 0) {
+                s.push(node);
+                s.push(new Node(node.n - 1, node.r));
+            } else {
+                this.arrBFS[node.n ][node.r] =
+                    this.arrBFS[node.n - 1][node.r - 1] + this.arrBFS[node.n - 1][node.r];
+            }
+        }
+
+        return this.arrBFS[n][r];
     }
 
     int[][] arr;
+    int[][] arrBFS;
 
     public static void main(String[] args) {
         Main T = new Main();
@@ -31,9 +77,11 @@ public class Main {
         int n = keyIn.nextInt();
         int r = keyIn.nextInt();
 
-        T.arr = new int[5][3];
+        T.arr = new int[n][r];
+        T.arrBFS = new int[n+1][r+1];
 
         System.out.println(T.solution(n, r));
+        System.out.println(T.BFS(n, r));
     }
 
 }
