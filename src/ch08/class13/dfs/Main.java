d651aea5fd2c3fde0679566980b512c4dce083c7
xeroman.kyn <xeroman.k@gmail.com>
2023-03-13 18:59:02 +0900

    230313 섬 몇개 역시나...chk 배열은 필요 없었고, 기존 배열을 0으로 변환해가며 움직이면 되는것...훨씬 간단하군.

--- src/ch08/class13/dfs/Main.java
+++ src/ch08/class13/dfs/Main.java
@@ -1,16 +1,17 @@
-package ch08.class13;
+package ch08.class13.dfs;
 
 import java.util.Scanner;
 
 public class Main {
 
-    public int solution(int n, int[][] arr, int[][] chk) {
+    public int solution(int n, int[][] arr) {
 
         int answer = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (arr[i][j] == 1 && chk[i][j] == 0) {
+                if (arr[i][j] == 1) {
                     answer++;
+                    arr[i][j] = 0;
                     DFS(n, i, j);
                 }
             }
@@ -20,16 +21,14 @@ public class Main {
     }
 
     public void DFS(int n, int i, int j) {
-        if (arr[i][j] == 1 && chk[i][j] == 0) {
-            chk[i][j] = 1;
 
-            for (int m = 0; m < 8; m++) {
-                int nI = i + moveX[m];
-                int nJ = j + moveY[m];
+        for (int m = 0; m < 8; m++) {
+            int nI = i + moveX[m];
+            int nJ = j + moveY[m];
 
-                if (nI > -1 && nI < n && nJ > -1 && nJ < n) {
-                    DFS(n, nI, nJ);
-                }
+            if (nI > -1 && nI < n && nJ > -1 && nJ < n && arr[nI][nJ] == 1) {
+                arr[nI][nJ] = 0;
+                DFS(n, nI, nJ);
             }
         }
     }
@@ -37,23 +36,20 @@ public class Main {
     static int[] moveX = {0, 0, 1, -1, 1, 1, -1, -1};
     static int[] moveY = {1, -1, 0, 0, 1, -1, -1, 1};
     static int[][] arr;
-    static int[][] chk;
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner keyIn = new Scanner(System.in);
         int n = keyIn.nextInt();
         arr = new int[n][n];
-        chk = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 arr[i][j] = keyIn.nextInt();
                 if (arr[i][j] == 0) {
-                    chk[i][j] = 1;
                 }
             }
         }
-        System.out.println(T.solution(n, arr, chk));
+        System.out.println(T.solution(n, arr));
     }
 
 }
